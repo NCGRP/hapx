f289dc19a2c337ea0abb24bf56bc17aec3921f39
Pat Reeves <pat.reeves@usda.gov>
2020-01-21 17:01:45 -0700

    end of day

--- README.txt
+++ README.txt
@@ -2,9 +2,21 @@ hapx extracts molecular haplotypes from bam files at user-specified sites
 
 In working folder:
 1) A fasta-formatted reference sequence
-2) A bam file containing reads aligned to the reference in (1)
+2) A sorted, deduped bam file containing reads aligned to the reference in (1)
 
 Requirements (in path):
 1) samtools (hapx calls samtools sort/view/mpileup)
 2) bwa (mem/index)
 3) sambamba (index)
+4) GNU parallel
+
+Usage: hapx ref bam sites
+where,
+ref = path to reference genome sequence in multi-fasta format
+bam = path to bam file of reads aligned to ref
+sites = genomic positions to use. Provide a comma-delimited list of the form:
+     jcf7180008454378:303-304,jcf7180008531951:103-495
+     which specifies bp 303-304 of the contig named "jcf7180008454378" and bps 103-495 of contig "jcf7180008531951".
+     Samtools view will not allow you to specify a single nucleotide position, so just use a 2bp range.
+
+Examples: ./hapx.sh /share/space/reevesp/patellifolia/ref/Ppanfinal.genome.scf.fasta /share/space/reevesp/patellifolia/xtr/AllP.merged.bam jcf7180008454378:303-304,jcf7180008531951:103-495;
--- hapx.sh
+++ hapx.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# Usage: go2pos ref sites
-# where,
-# ref = path to reference genome sequence in multi-fasta format
-# sites = genomic positions to use. Provide a comma-delimited list of the form:
-      jcf1.75000:jcf1.1000000,jcf14.8697509:jcf14.8697509
-      which specifies bp 75000-1000000 (inclusive) of the contig named "jcf1" and bp 8697509 of contig "jcf14".
+# Usage: see README.txt
 
 ##SUBROUTINES##
 
@@ -124,44 +119,14 @@ export -f myinsertion;
 ##END SUBROUTINES##
 
 
-
-
 #acquire command line variables
-ref=$1; #the reference genome sequence in multi-fasta format
-sites=$2; #genomic regions to use
-
-
-#randomly sample haplotypes at a few positions
-#get list of contigs and length, shuffle it for random sampling (samtools idxstats output: ref,length,mapped reads,unmapped reads)
-samtools idxstats AllP.merged.bam | shuf > AllP.idxstats.txt; #plot distribution in ReadsPerContig.png, chloroplast sequences have very high reads per contig (>1E6 reads <20kb contig)
-
-nl=10; #number of loci desired, these will be on different contigs
-a=$(head -$nl AllP.idxstats.txt);
-
-#select a site at random from each contig
-b=$(for i in $(echo "$a" | cut -d$'\t' -f2 | tr "\n" " ");
-  do shuf -i 1-"$i" -n 1; #use shuf to emit one random number from the range
-  done;
-);
-
-c=$(paste -d$'\t' <(echo "$a") <(echo "$b")); #place random site in column 5
-echo "$c" > AllP.randomsites"$nl".txt; #save the randomly chosen sites in case you need them later
-
-#get list like contig name:random site-random site +1 (e.g. jcf7180008527965:177-178), substitute file for "$c" AllP.randomsites"$nl".txt input if variable has been lost
-e=$(while read j;
-do d=$(echo "$j" | cut -d$'\t' -f1,5 | tr "\t" ":");
-  dd=$(( $(echo "$d" | cut -d: -f2) + 1 ));
-  echo "$d"-"$dd"; #format like contig1:167-168
-done <<<"$c";)
-
-
-
-####START HERE REPEATING WITH PROPER REGION SPECIFICATION (in $e)
-
-
+ref=$1; #path to the reference genome sequence in multi-fasta format
+aln=$2; #path to the bam file containing the alignment of reads to ref
+sites=$3; #genomic regions to use
+e=$(echo "$sites" | tr "," "\n"); #format sites for proper parsing by samtools view
 
 #extract read pairs at site using samtools. Extract proper pairs with mapping quality > 60 excluding unmapped pairs
-echo "$e" | parallel 'samtools view -f 2 -F 4 -q 60 AllP.merged.bam "{}" | sort > {}.tmp; \
+echo "$e" | parallel 'samtools view -f 2 -F 4 -q 60 '"$aln"' "{}" | sort > {}.tmp; \
   mv {}.tmp $(echo {} | tr ":" "_").tmp';
   
 #describe number of read pairs and single ends of a read pair extracted in a tabular format
@@ -174,57 +139,51 @@ f=$(while read j;
     ss=$(echo "$s" | awk 'NF' | wc -l); #number of single ends of paired reads
     echo "$j $pp $ss";
   done <<<"$e";)
-echo "contig:site pairedreads singlereads"$'\n'"$f";
+echo "Number of reads found at sites:"$'\n'"contig:sites pairedreads singlereads"$'\n'"$f"$'\n';
 
-			jcf7180008454378:303-304 0 8
-			jcf7180008531951:103-104 4 45
-			jcf7180008395354:294-295 3 55
-			jcf7180008827236:1031-1032 0 19
-			jcf7180008378511:277-278 4 39
-			jcf7180008637475:7-8 0 2
-			jcf7180008587925:106-107 2 7
-			jcf7180008527965:177-178 1 17
-			jcf7180008578969:84-85 7 39
-			jcf7180008484650:470-471 0 7
+#           sample output:
+#           Number of reads found at sites
+#           contig:sites pairedreads singlereads
+#           jcf7180008454378:303-304 0 8
+#           jcf7180008531951:103-495 12 94
 
 
 g=$(echo "$f" | awk -F' ' '$2+$3!=0 {print $0}'); #remove any contigs from consideration when there are 0 reads (no paired reads:$2, no unpaired reads:$3) that align to them (this could also be a variable that supports a cutoff)
 
 #extract each contig from the reference genome to be used by itself as a reference for realignment with the extracted read pairs, then bwa index
-echo "$g" | cut -d: -f1 | parallel 'sed -n -e "/^>{}$/,/>/p" Ppanfinal.genome.scf.fasta | sed "$ d" > {}_ref.txt; bwa index {}_ref.txt'; #sed extracts lines from name of contig of interest until next contig name line, second sed deletes the last line
+echo "$g" | cut -d: -f1 | parallel 'sed -n -e "/^>{}$/,/>/p" '"$ref"' | sed "$ d" > {}_ref.txt; bwa index {}_ref.txt'; #sed extracts lines from name of contig of interest until next contig name line, second sed deletes the last line
 
 #reconstruct fastq file for reads that aligned to each contig, adding readgroup and f/r designation, r is for reads marked with a negative alignment length in column 9
 #then sort reads into individual read pairs, resulting files are like {contigname_readname.rp.fq}
-#rm *rp.fq; #you should remove the old read pair output files before proceeding here
+echo "Reconstructing fastq files for reads found at sites:";
 for i in $(echo "$g" | cut -d' ' -f1 | tr ':' '_' | tr '\n' ' ');
   do awk -F$'\t' '{print "@"$1,$9,$10,"+",$11,$17}' "$i".tmp | awk -F' ' '{if ($2 > 0) {print $1"_"$6":f",$3,$4,$5} else if ($2 < 0) {print $1"_"$6":r",$3,$4,$5}}' | tr " " "\n" > "$i"_reads.fq;
 
     m=$(grep ^'@' "$i"_reads.fq | cut -d: -f1-9 | sort -u); #capture list of unique read pair names (includes also unpaired reads with unique names) 
     for j in $m;
-      do echo "$i / $j";
+      do echo "$i $j";
         fn=$(echo "$j" | sed 's/@//g' | tr ':' '_'); #filename for output
-        grep -A3 "$j" "$i"_reads.fq > "$i"_"$fn".rp.fq; #save read pair into its own file
+        grep -A3 "$j" "$i"_reads.fq > "$i"_"$fn".rp.fq; #save read pair into its own reconstructed fastq file
       done;
     
     rm "$i"_reads.fq; #clean up
     rm "$i".tmp;
   done;
 
-			Here is a sample reconstructed *.rp.fq file, note readgroup in col9 of name, and "orientation" or "endedness of read pair" in col10:
-			@E00558:144:HHGCMCCXY:5:2224:6654:40055_RG:Z:55:r
-			CAAAAATATTTTGGTAATTATTCTCAACAAAATGATTTGAAAGGTGTTCATACAACACAAATCGCCTAAGAGACTATGACGGTTTTATCCTCTGATTTGAATTGAGTTTGATCCAAGGGCTTCATATGATTGAAATATAC
-			+
-			FFJJJJAFJ<JJFF7FJAJAA-A7AJJJJJJ<JJJJJJJJJJFJJJJJJJJJJJ<JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJFFFAA
-			@E00558:144:HHGCMCCXY:5:2224:6654:40055_RG:Z:55:f
-			ATAAATATGCAAGGAGTCAAAAATATTTGGTAATTAAGCACAAAAACTGATTTGAAATGTGTTCGTACACCACAAATCACCTAAGAGACTATGACGGTTTTACCCTTTGATTTGAATTGAGTTTGATCCAAGGGCTTCATATGCTTTAAA
-			+
-			AAFFFJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJFJJJJJFFJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJFJJJJJJJJJJJJJ
+#           Here is a sample reconstructed *.rp.fq file, note readgroup in col9 of name, and "orientation" or "endedness of read pair" in col10:
+#          @E00558:144:HHGCMCCXY:5:2224:6654:40055_RG:Z:55:r
+#          CAAAAATATTTTGGTAATTATTCTCAACAAAATGATTTGAAAGGTGTTCATACAACACAAATCGCCTAAGAGACTATGACGGTTTTATCCTCTGATTTGAATTGAGTTTGATCCAAGGGCTTCATATGATTGAAATATAC
+#          +
+#          FFJJJJAFJ<JJFF7FJAJAA-A7AJJJJJJ<JJJJJJJJJJFJJJJJJJJJJJ<JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJFFFAA
+#          @E00558:144:HHGCMCCXY:5:2224:6654:40055_RG:Z:55:f
+#          ATAAATATGCAAGGAGTCAAAAATATTTGGTAATTAAGCACAAAAACTGATTTGAAATGTGTTCGTACACCACAAATCACCTAAGAGACTATGACGGTTTTACCCTTTGATTTGAATTGAGTTTGATCCAAGGGCTTCATATGCTTTAAA
+#          +
+#          AAFFFJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJFJJJJJFFJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJFJJJJJJJJJJJJJ
 
 #align *.rp.fq to *_ref.txt, output as bam file, then sambamba index (output files have names like jcf7180008378511_277.bam (contigname_site.bam)
 #this step aligns, then verifies mapping quality since it changes from the original values, then realigns
-#rm *_RG_Z_5[012345].[bs]am*; #clean up old files before repeating this step
-#rm *2x*;
 #This step should probably be parallelized
+echo "Realigning reads:"
 for i in $(find . -name "*.rp.fq" | cut -d'/' -f2 | sed 's/\.rp\.fq//g' | tr "\n" " ");
   do echo "$i";
     h=$(echo "$i" | cut -d_ -f1); #just the contig name
@@ -245,36 +204,27 @@ for i in $(find . -name "*.rp.fq" | cut -d'/' -f2 | sed 's/\.rp\.fq//g' | tr "\n
   done;
 
 
+#turn on some diagnostic code by un-commenting:
 #print mapping quality after 1st realignment (it changes from original mapping quality)
-awk -F$'\t' '{print $5}' *.sam | grep -v ^CL | grep -v ^$ | sort | uniq -c;
+#awk -F$'\t' '{print $5}' *.sam | grep -v ^CL | grep -v ^$ | sort | uniq -c;
 #print mapping quality after RE-realignment (all 60)
-awk -F$'\t' '{print $5}' *.2x.sam | grep -v ^CL | grep -v ^$ | sort | uniq -c;
+#awk -F$'\t' '{print $5}' *.2x.sam | grep -v ^CL | grep -v ^$ | sort | uniq -c;
 
 #figure out the possibilities for coding in the nucleotide column ($5)
-> xxx.tmp;
-for i in $(find . -name "jcf*_RG_Z_5[012345].2x.sam");
-  do echo "$i" >> xxx.tmp;
-    samtools mpileup "$i" >> xxx.tmp;
-  done;
-awk -F$'\t' '{print $4,$5}' xxx.tmp | sort | uniq -c; #print all unique possibilities for nucleotide coding, these have to be accounted for when assembling the consensus
-
-
-
-
-
-
-
-
-
-
-
-
+#> xxx.tmp;
+#for i in $(find . -name "jcf*_RG_Z_5[012345].2x.sam");
+#  do echo "$i" >> xxx.tmp;
+#    samtools mpileup "$i" >> xxx.tmp;
+#  done;
+#awk -F$'\t' '{print $4,$5}' xxx.tmp | sort | uniq -c > ntcodes.tmp; #print all unique possibilities for nucleotide coding, these have to be accounted for when assembling the consensus
 
 
 
 
 
 #Process alignments of paired reads via mpileup into a single padded consensus sequence representing the haplotype
+if [ ! -d "haplotypes" ]; then mkdir "haplotypes"; fi; #make a directory to hold fasta sequences containing extracted haplotypes
+
 #Could also parallelize this
 for i in $(find . -name "*.2x.sam" | tr "\n" " ");
   do samin="$i";
@@ -305,8 +255,24 @@ for i in $(find . -name "*.2x.sam" | tr "\n" " ");
     #base3 contains the processed haplotype as a fasta file, deletions relative to reference removed.
     #base3 is as close as we can come to reconstructing the native molecule
     base3=$(echo ">$rname";echo "$base2" | cut -d' ' -f2 | grep -v '-' | tr "\n" " " | sed 's/ //g');
-    echo "$base3" > "$rname.fa";
+    echo "$base3" > haplotypes/"$rname.fa";
     
     #clean up
     rm "$samin";
   done;
+
+
+
+
+
+#Produce an alignment of the haplotypes to their reference, which might be viewed in something like IGV (Integrative Genomics Viewer)
+if [ ! -d "alignments" ]; then mkdir "alignments"; fi; #make a directory to hold fasta sequences containing extracted haplotypes
+ams=$(find ./haplotypes -name "*.2x.fa" | cut -d_ -f1-2 | sort -u | rev | cut -d'/' -f1 | rev);
+echo "$ams" | parallel 'cat ./haplotypes/{}_*.2x.fa > ./alignments/{}.mfa'; #concatenate all fasta haplotypes belonging to a single contig:range
+#start here, do bwa-mem in parallel with *_ref.txt
+
+
+#clean up
+rm *_ref.txt*;
+
+  
