0ef1080fc09f1da1e294c6eb8503f0d552b6522f
Toledo <3apotencia@gmail.com>
2020-04-01 14:56:04 -0600

    trying to deal with non-standard characters in contig names

--- hapx.sh
+++ hapx.sh
@@ -451,9 +451,9 @@ echo "$reportctsfreqs" | sed '/^$/d'; #report to parallel statement
        #remake the global output file, with nothing removed, if -d option not selected. Otherwise, when $dodedup == "YES" (-d switch on), make the global output file with identical (sub)sequences removed
        #only do any of this if the user has agreed to print output
        if [[ "$nooutput" == "NO" ]] && [[ "$dodedup" == "NO" ]]; 
-       then sed -e '/^>/s/$/@/' -e 's/^>/#/' <(echo "$mfa") | tr -d '\n' | tr "#" "\n" | tr "@" " " | sed '/^$/d' | awk '{print ">rp" NR "_" $0}' | tr " " "\n" > "$pd"/alignments/"$i".global.fa; #produce undeduped output for alignment
+       then sed -e '/^>/s/$/@/' -e 's/^>/#/' <(echo "$mfa") | tr -d '\n' | tr "#" "\n" | tr "@" " " | sed '/^$/d' | awk '{print ">rp" NR "_" $0}' | tr " " "\n" > "$pd"/alignments/"$inf".global.fa; #produce undeduped output for alignment
        elif [[ "$nooutput" == "NO" ]] && [[ "$dodedup" == "YES" ]]
-       then echo "$fonfa" > "$pd"/alignments/"$i".global.fa; #produce deduped output for alignment
+       then echo "$fonfa" > "$pd"/alignments/"$inf".global.fa; #produce deduped output for alignment
        fi;
 
 
